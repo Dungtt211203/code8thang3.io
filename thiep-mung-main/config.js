f4c0f303341c8b06f39bd2ee6291883fad0416f9
Dungtt211203 <100980763+Dungtt211203@users.noreply.github.com>
2024-03-03 19:39:20 +0700

    Add files via upload

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100060079937536';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng cậu ';
+const contentCard = 'Chúc các chị em đồng nghiệp ngày Quốc tế Phụ nữ vui vẻ, luôn xinh tươi và nhận được thật nhiều quà từ phái nam nhé.';
+const giftImage = 'anhdaidienthu1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
